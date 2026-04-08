584a1d0cc3970c1e0a1a1af6154632f515114ec7
HappyKarhu <mojca.jan@gmail.com>
2026-04-08 09:52:46 +0300

    routes in nav link

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,26 +1,56 @@
-<header class="bg-emerald-600 text-white p-4"x-data="{ open: false }">
-  <div class="container mx-auto flex justify-between items-center">
-    <h1 class="text-3xl font-semibold">
-      <x-nav-link url="{{ url('/') }}" :active="request()->is('/')">
-        Volunteerio
-      </x-nav-link>
-    </h1>
-    <nav class="hidden md:flex items-center space-x-4">
-      <x-nav-link :url="url('/events')" :active="request()->is('events')">All Events</x-nav-link>
-      <x-nav-link :url="url('/events/saved')" :active="request()->is('events/saved')">Saved Events</x-nav-link>
-      <x-nav-link :url="url('/login')" :active="request()->is('login')" icon="user">Login</x-nav-link>
-      <x-nav-link :url="url('/register')" :active="request()->is('register')">Register</x-nav-link>
-      <x-nav-link :url="route('events.create')" :active="request()->is('events/create')" icon="seedling">Create event</x-nav-link>
-	
-      <x-button-link :url="url('/dashboard')" :active="request()->is('dashboard')" icon="clipboard-user">My Dashboard</x-button-link>
-    </nav>
-    <button @click="open = !open"
-                    
-                    class="text-white md:hidden flex items-center"
-                >
-                    <i class="fa fa-bars text-2xl"></i>
-                </button>
-  </div>
+<header class="bg-emerald-600 text-white p-4" x-data="{ open: false }">
+    <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-3xl font-semibold">
+            <x-nav-link :url="url('/')" :active="request()->is('/')">
+                Volunteerio
+            </x-nav-link>
+        </h1>
+
+        <!--Public navigation links-->
+        <nav class="hidden md:flex items-center space-x-4">
+            <x-nav-link :url="route('events.index')" :active="request()->is('events')">
+                All Events
+            </x-nav-link>
+
+            <!--Authenticated user links : dashboard is seen by organizers and volunteers -->
+            @auth
+                <!--Volunteer-specific links-->
+                @if(auth()->user()->role === 'volunteer')
+                <x-nav-link :url="route('events.saved')" :active="request()->is('events/saved')">
+                    Saved Events
+                </x-nav-link>
+                @endif
+
+                <!--Organizer-specific links-->
+                @if(auth()->user()->role === 'organizer')
+                    <x-nav-link :url="route('events.create')" :active="request()->is('events/create')" icon="seedling">
+                        Create Event
+                    </x-nav-link>
+                @endif
+
+                <x-button-link :url="route('dashboard')" :active="request()->is('dashboard')" icon="clipboard-user">
+                  My Dashboard
+                </x-button-link>
+
+            @endauth
+
+            <!--Public navigation links-->
+            @guest
+                <x-nav-link :url="url('/login')" :active="request()->is('login')" icon="user">
+                    Login
+                </x-nav-link>
+
+                <x-nav-link :url="url('/register')" :active="request()->is('register')" icon="user-plus">
+                    Register
+                </x-nav-link>
+            @endguest
+        </nav>
+
+        <button @click="open = !open" class="text-white md:hidden flex items-center">
+            <i class="fa fa-bars text-2xl"></i>
+        </button>
+    </div>
+
 
   <!-- Mobile Menu -->
 <nav
@@ -28,12 +58,46 @@
   id="mobile-menu"
   class="md:hidden bg-emerald-600 text-white mt-5 pb-4 space-y-2"
 	>
-	  <x-nav-link :url="url('/events')" :active="request()->is('events')">All Events</x-nav-link>
-	      <x-nav-link mobile="true" :url="url('/events/saved')" :active="request()->is('events/saved')">Saved Events</x-nav-link>
-	      <x-nav-link mobile="true" :url="url('/login')" :active="request()->is('login')" icon="user">Login</x-nav-link>
-	      <x-nav-link mobile="true" :url="url('/register')" :active="request()->is('register')">Register</x-nav-link>
-	      <x-nav-link mobile="true" :url="route('events.create')" :active="request()->is('events/create')" icon="seedling">Create event</x-nav-link>
-	
-	      <x-button-link mobile="true" block="true" :url="url('/dashboard')" :active="request()->is('dashboard')" icon="clipboard-user">My Dashboard</x-button-link>
-	    </nav>
+	  <nav x-show="open" @click.away="open = false" id="mobile-menu" class="md:hidden bg-emerald-600 text-white mt-5 pb-4 space-y-2">
+
+      <!--Public navigation links-->
+        <x-nav-link mobile="true" :url="route('events.index')" :active="request()->is('events')">
+            All Events
+        </x-nav-link>
+
+        <!--Authenticated user links-->
+        @auth
+
+            <!--Volunteer-specific links-->
+            @if(auth()->user()->role === 'volunteer')
+            <x-nav-link mobile="true" :url="route('events.saved')" :active="request()->is('events/saved')">
+                Saved Events
+            </x-nav-link>
+            @endif
+
+            <!--Organizer-specific links-->
+            @if(auth()->user()->role === 'organizer')
+                <x-nav-link mobile="true" :url="route('events.create')" :active="request()->is('events/create')" icon="seedling">
+                    Create Event
+                </x-nav-link>
+            @endif
+
+            <x-button-link mobile="true" block="true" :url="route('dashboard')" :active="request()->is('dashboard')" icon="clipboard-user">
+                My Dashboard
+            </x-button-link>
+  
+
+        @endauth
+
+        <!--Public navigation links-->
+        @guest
+            <x-nav-link mobile="true" :url="url('/login')" :active="request()->is('login')" icon="user">
+                Login
+            </x-nav-link>
+
+            <x-nav-link mobile="true" :url="url('/register')" :active="request()->is('register')">
+                Register
+            </x-nav-link>
+        @endguest
+    </nav>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -8,46 +8,47 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\BookmarkController;
 
+//public routes
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/pages', [HomeController::class, 'index'])->name('pages.index');
 Route::get('/events', [EventController::class, 'index'])->name('events.index');
 
-// User routes
+//user public profile
 Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
-Route::post('/events', [EventController::class, 'store'])->name('events.store');
-    
+
+//dashboard routes (loggin required)
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+//profile routes (logged in users only)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
     });
 
-Route::middleware(['auth', 'role:organizer'])->group(function () {
-    Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
-    Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update');
-    Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
-    
-});
-
-Route::middleware(['auth', 'role:organizer'])->group(function () {
-    Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
-    Route::post('/events', [EventController::class, 'store'])->name('events.store');
-    
-    Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
-    Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update');
-    Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
-});
-
+//bookmark routes (logged in users only)    
 Route::middleware('auth')->group(function () {
     Route::get('/events/saved', [BookmarkController::class, 'index'])->name('events.saved');
     Route::post('/events/{event}/save', [BookmarkController::class, 'store'])->name('events.save');
     Route::delete('/events/{event}/save', [BookmarkController::class, 'destroy'])->name('events.unsave');
+});
+
+//orginizer routes (logged in organizers only)
+Route::middleware(['auth', 'role:organizer'])->group(function () {
+    Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
+    Route::post('/events', [EventController::class, 'store'])->name('events.store');
+
+    Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
+    Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update');
+    Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
+    
+});
+
+//volunteer routes (logged in volunteers only)
+Route::middleware(['auth', 'role:volunteer'])->group(function () {
+    Route::post('/events/{event}/apply', [EventController::class, 'apply'])->name('events.apply');
 });
 
 Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
